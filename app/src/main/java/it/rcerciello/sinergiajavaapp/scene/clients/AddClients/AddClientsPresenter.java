92f91eecddf1ec1d3bb475587f7e2eb92b671a95
Cerciello, Raffaella (IT - Milano) <rcerciello@deloitte.it>
2018-05-05 20:58:51 +0200

    First commit

--- app/src/main/java/it/rcerciello/sinergiajavaapp/scene/clients/AddClients/AddClientsPresenter.java
+++ app/src/main/java/it/rcerciello/sinergiajavaapp/scene/clients/AddClients/AddClientsPresenter.java
@@ -2,7 +2,11 @@ package it.rcerciello.sinergiajavaapp.scene.clients.AddClients;
 
 import javax.annotation.Nonnull;
 
+import it.rcerciello.sinergiajavaapp.R;
+import it.rcerciello.sinergiajavaapp.Sinergia;
+import it.rcerciello.sinergiajavaapp.data.managers.ClientNetworkLayer;
 import it.rcerciello.sinergiajavaapp.data.modelli.ClientModel;
+import it.rcerciello.sinergiajavaapp.data.network.APICallback;
 
 /**
  * Created by rcerciello on 02/05/2018.
@@ -33,6 +37,28 @@ public class AddClientsPresenter implements AddClientsContract.Presenter {
 
     @Override
     public void addClient(ClientModel model) {
+        ClientNetworkLayer.getInstance().addClient(model, new APICallback<Boolean>() {
+            @Override
+            public void onSuccess(Boolean object) {
+                if (object) {
+                    mView.closeView();
+                } else {
+                    mView.showSnackbarError(Sinergia.INSTANCE.getResources().getString(R.string.something_went_wrong));
+                }
+            }
+
+            @Override
+            public void onFailure(String error) {
+                if (error != null) {
+                    mView.showSnackbarError(error);
+                }
+            }
+
+            @Override
+            public void onSessionExpired() {
+
+            }
+        });
 
     }
 }
